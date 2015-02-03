34008b7bb235fa34088937f7b5d43a699a98d610
Declan Curran <entityingredient@gmail.com>
2015-02-03 18:32:53 +0000

    Moved web application to subdirectory

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, request, redirect, url_for, jsonify, render_template, session, g
+from github import GitHub
+import time
+
+app = Flask(__name__)
+app.config.from_pyfile('config.cfg')
+
+def error(code, msg):
+	return (render_template('error.html', error={'code': code, 'message': msg}), code)
+
+# Catch all requests
+@app.after_request
+def after_req(resp):
+	if 'access_token' in session:
+		session['last_visit'] = time.time()
+	return resp
+
+@app.before_request
+def before_req():
+	# Set up GitHub API
+	g.github = GitHub(
+		app.config.get('GITHUB_CLIENT_ID'),
+		app.config.get('GITHUB_CLIENT_SECRET'),
+		'github.com/dcrn/fyp'
+	)
+
+	if 'access_token' in session and \
+		(time.time() - session.get('last_visit', 0)) > app.config.get('ACCESS_TOKEN_UPDATE_TIME'):
+
+		# Validate the user's access token
+		status, resuser = g.github.get_user(session.get('access_token'))
+		if status: 	# Update user details
+			session['user_info'] = resuser
+		else:		# Clear session info if access token was revoked
+			session.clear()
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+  
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	code = request.args.get('code')
+	if code is None:
+		return error(403, 'Forbidden')
+	else:
+		status, restoken = g.github.get_access_token(code)
+
+		if not status:
+			return error(500, 'Internal Server Error')
+		if 'access_token' not in restoken:
+			return error(401, 'Unauthorized')
+
+		status, resuser = g.github.get_user(restoken['access_token'])
+
+		if not status:
+			return error(500, 'Internal Server Error')
+		
+		# Set up session variables
+		session['access_token'] = restoken['access_token']
+		session['user_info'] = resuser
+
+	return redirect(url_for('index'))
+
+@app.route('/logout')
+def logout():
+	session.clear()
+	return redirect(url_for('index'))
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', debug=True);
---
+++ github.py
@@ -0,0 +1,64 @@
+import urllib.request, json
+
+class GitHub:
+	githubaddr = 'https://github.com/'
+	apiaddr = 'https://api.github.com/'
+
+	def __init__(self, client_id, client_secret, useragent):
+		self.client_id = client_id
+		self.client_secret = client_secret
+		self.useragent = useragent
+
+	def api_call(self, path, access_token=None, post_json=None, addr=apiaddr, raw_output=False, headers=[], get_params={}):
+		if not access_token: # Authorize with Application details if no access token provided
+			get_params['client_id'] = self.client_id
+			get_params['client_secret'] = self.client_secret
+
+		# Build URL and request
+		url = addr + path + '?' + urllib.parse.urlencode(get_params)
+		req = urllib.request.Request(url)
+
+		# If access token is used, add the authorization header
+		if access_token:
+			req.add_header('Authorization', 'token ' + access_token)
+
+		# Add any post data to the request
+		if post_json:
+			req.data = bytes(json.dumps(post_json), 'utf-8')
+
+		# Add User-Agent (GitHub API requirement)
+		req.add_header('User-Agent', self.useragent)
+
+		for v in headers:
+			req.add_header(*v)
+
+		response = None
+		try:
+			response = urllib.request.urlopen(req)
+		except Exception as e:
+			return (False, e)
+
+		if response:
+			data = response.readall().decode('utf-8')
+			if raw_output:
+				return (True, data)
+			else:
+				return (True, json.loads(data))
+		else:
+			return (False, None)
+
+	def rate_limit(self, access_token=None):
+		if access_token:
+			return self.api_call('rate_limit', access_token=access_token)
+		else:
+			return self.api_call('rate_limit')
+
+	def get_access_token(self, code):
+		return self.api_call('login/oauth/access_token',
+				get_params={'code': code},
+				addr=self.githubaddr,
+				headers=[('Accept', 'application/json')]
+			)
+
+	def get_user(self, access_token):
+		return self.api_call('user', access_token=access_token)
